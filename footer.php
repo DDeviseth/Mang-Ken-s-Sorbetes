5ba277faad7fbc861c8a32af1cb5beb505bbcadc
Kenaz <yanezkenazbrian@gmail.com>
2025-12-07 19:04:11 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
-    <hr>
     <footer>
-        <p>Maraming salamat sa order, <strong><?= $customer_name ?></strong>!</p>
+        <p><strong>Maraming salamat sa pag-order!</strong></p>
         <p>Mang Ken Sorbetes © <?= date("Y") ?> – by Kenaz Brian Yanez</p>
     </footer>
 </div>
