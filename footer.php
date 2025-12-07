0ea40705d0e5a4463da014c8fa58948e8627a766
Kenaz <yanezkenazbrian@gmail.com>
2025-12-07 19:05:15 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-    <footer>
-        <p><strong>Maraming salamat sa pag-order!</strong></p>
-        <p>Mang Ken Sorbetes © <?= date("Y") ?> – by Kenaz Brian Yanez</p>
-    </footer>
-</div>
-</body>
-</html>
